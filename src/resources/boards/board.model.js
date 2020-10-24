3ae5bea69c08035f5ef461f5d444fc43fda2ea16
kastrubait <60570588+kastrubait@users.noreply.github.com>
2020-10-24 09:47:55 +0300

    refactor: Board model changed

--- src/resources/boards/board.model.js
+++ src/resources/boards/board.model.js
@@ -1,16 +1,23 @@
-const uuid = require('uuid');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-class Board {
-  constructor({ id = uuid(), title = 'Title_BOARD', columns = [] } = {}) {
-    this.id = id;
-    this.title = title;
-    this.columns = columns;
-  }
+const Board = new Schema(
+  {
+    title: String,
+    columns: {
+      type: Array,
+      default: []
+    }
+  },
+  { collections: 'boards' }
+);
 
-  static toResponse(board) {
-    const { id, title, columns } = board;
-    return { id, title, columns };
-  }
-}
+const toResponse = board => {
+  const { id, title, columns } = board;
+  return { id, title, columns };
+};
 
-module.exports = Board;
+module.exports = {
+  Board: mongoose.model('boards', Board),
+  toResponse
+};
