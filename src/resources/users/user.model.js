61658b590ffc08a4ec1507fa7bd8f92235aabdec
kastrubait <60570588+kastrubait@users.noreply.github.com>
2020-10-24 07:29:38 +0300

    refactor: User model changed

--- src/resources/users/user.model.js
+++ src/resources/users/user.model.js
@@ -1,22 +1,21 @@
-const uuid = require('uuid');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-class User {
-  constructor({
-    id = uuid(),
-    name = 'USER',
-    login = 'user',
-    password = 'P@55w0rd'
-  } = {}) {
-    this.id = id;
-    this.name = name;
-    this.login = login;
-    this.password = password;
-  }
+const User = new Schema(
+  {
+    name: String,
+    login: String,
+    password: String
+  },
+  { collections: 'users' }
+);
 
-  static toResponse(user) {
-    const { id, name, login } = user;
-    return { id, name, login };
-  }
-}
+const toResponse = user => {
+  const { id, name, login } = user;
+  return { id, name, login };
+};
 
-module.exports = User;
+module.exports = {
+  User: mongoose.model('users', User),
+  toResponse
+};
