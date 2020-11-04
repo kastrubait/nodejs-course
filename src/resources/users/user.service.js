f88b7d13ac52eedfec022c7736882983068f6313
kastrubait <60570588+kastrubait@users.noreply.github.com>
2020-11-04 21:51:24 +0300

    feat: init task5, add POST/users which, takes a password field and replace it with a hash

--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,14 +1,22 @@
 const usersRepo = require('./user.DB.repository');
 const tasksRepo = require('../tasks/task.DB.repository');
+const { hashPassword } = require('../../utils/hashHelper');
 
 const getAll = async () => await usersRepo.getAll();
 
 const get = async userId => await usersRepo.get(userId);
 
-const create = async userData => await usersRepo.create(userData);
+const create = async userData => {
+  const password = await hashPassword(userData);
+  const res = await usersRepo.create({ ...userData, password });
+  return res;
+};
 
-const update = async (userId, userData) =>
-  await usersRepo.update(userId, userData);
+const update = async (userId, userData) => {
+  const password = await hashPassword(userData);
+  const res = await usersRepo.update(userId, { ...userData, password });
+  return res;
+};
 
 const remove = async userId => {
   const getAllTask = await tasksRepo.getAll();
